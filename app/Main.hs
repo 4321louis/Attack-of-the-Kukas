b3bcd09d88d762931b909f0a59efe6d98f0ef4ff
4321louis <45321dumkof@gmail.com>
2023-01-31 03:33:55 +1100

    Go pew on start up for lucas

--- app/Main.hs
+++ app/Main.hs
@@ -34,6 +34,13 @@ import Debug.Trace (trace)
 import Debug.Time
 import Enemy.Pathfinding
 
+import System.Environment
+import System.FilePath
+import qualified Data.ByteString as SB
+import Control.Concurrent
+
+import Sound.ProteaAudio
+
 
 makeWorld "World" [''Position, ''Velocity, ''MovementPattern, ''Paths, ''PathFinder, ''Sprite, ''AnimatedSprite, ''Player, ''Bullet, ''Particle, ''Score, ''Time, ''Inputs, ''Camera]
 
@@ -111,8 +118,54 @@ draw bg = do
 
     return $ bg <> sprites <> score <> particles
 
+waitPlayback = do
+  n <- soundActiveAll
+  when  (n > 0) $ do
+    threadDelay oneSec
+    waitPlayback
+
+oneSec :: Int
+oneSec = 1000000 -- micro seconds
+
+tempAudioMain = do
+    let filename = "./src/menuLoop.wav" 
+
+    result <- initAudio 64 44100 1024 -- max channels, mixing frequency, mixing buffer size
+    unless result $ fail "failed to initialize the audio system"
+
+    -- (A) load sample from file
+    sampleA <- sampleFromFile filename 1.0 -- volume
+
+    -- start two sound tracks with shared sample data
+    sndTrkA <- soundPlay sampleA 1 1 0 1 -- left volume, right volume, time difference between left and right, pitch factor for playback
+    threadDelay oneSec -- wait 1 sec
+    sndTrkB <- soundPlay sampleA 1 1 0 1 -- left volume, right volume, time difference between left and right, pitch factor for playback
+    soundActive sndTrkB >>= print
+    -- play 3 sec
+    threadDelay $ 3 * oneSec
+    soundStop sndTrkB
+    soundActive sndTrkB >>= print
+    -- wait sndTrkA to finish
+    waitPlayback
+
+    -- (B) load from memory buffer
+    buffer <- SB.readFile filename
+    sampleB <- case takeExtension filename of
+        ".ogg" -> sampleFromMemoryOgg buffer 1.0
+        ".wav" -> sampleFromMemoryWav buffer 1.0
+
+    soundPlay sampleB 1 1 0 1 -- left volume, right volume, time difference between left and right, pitch factor for playback
+    waitPlayback
+
+    sampleDestroy sampleB
+    soundPlay sampleB 1 1 0 1 -- we have invalidated the handle; nothing should happen now
+    waitPlayback
+
+    finishAudio
+
 main :: IO ()
 main = do
+    tempAudioMain
     content <- readFile "./src/meta.txt"
     let size = traceTimer "WFCollapse" 50
         tileOptions = readTilesMeta content
