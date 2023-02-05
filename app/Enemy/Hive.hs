110d13dad1afe33e47d12fcde0ae72ed48503a8e
Lucas Rodiadis <lucas2@live.com.au>
2023-02-05 17:56:39 +1100

    partially did enemies

--- app/Enemy/Hive.hs
+++ app/Enemy/Hive.hs
@@ -44,114 +44,46 @@ spawnsHive :: (HasMany w [Time, Hp, Enemy, Position, Sprite, AnimatedSprite, Vel
 spawnsHive Hive1 dT pos = do
     spawnWave 20 dT 3 1.3 $ getEnemyFromType Normal 1 pos
     spawnWave 40 dT 3 1.3 $ getEnemyFromType Normal 1 pos
-    doMinute 2 dT 3 3 pos Normal
+
     doMinute 4 dT 3 3 pos Normal
-    doMinute 5 dT 5 3 pos Normal
-    doMinute 6 dT 3 3 pos Normal
-    doMinute 14 dT 3 3 pos Normal
-    doMinute 15 dT 3 3 pos Normal
-    doMinute 16 dT 8 3 pos Normal
-    doMinute 18 dT 8 3 pos Normal
-    doMinute 19 dT 8 3 pos Normal
-    doMinute 20 dT 8 3 pos Normal
-    doMinute 21 dT 5 3 pos Normal
-    doMinute 22 dT 5 3 pos Normal
-    doMinute 23 dT 5 3 pos Normal
-    doMinute 24 dT 8 3 pos Normal
-    doMinute 25 dT 5 3 pos Normal
-    doMinute 26 dT 5 3 pos Normal
-    doMinute 27 dT 8 3 pos Normal
-    doMinute 28 dT 8 3 pos Normal
-    doMinute 29 dT 8 3 pos Normal
+    doMinute 7 dT 8 3 pos Normal
+    doMinute 8 dT 3 3 pos Normal
+    doMinute 10 dT 3 3 pos Fast
+    doMinute 13 dT 8 3 pos Normal
+    doMinute 15 dT 5 3 pos Fast
 
 spawnsHive Hive2 dT pos = do
     doMinute 1 dT 3 3 pos Normal
     doMinute 4 dT 3 3 pos Normal
-    doMinute 6 dT 5 3 pos Normal
     doMinute 7 dT 5 3 pos Normal
-    doMinute 10 dT 5 3 pos Normal
-    doMinute 11 dT 5 3 pos Normal
-    doMinute 12 dT 5 3 pos Normal
-    doMinute 13 dT 8 3 pos Normal
-    doMinute 14 dT 5 3 pos Normal
-    doMinute 15 dT 3 3 pos Normal
-    doMinute 16 dT 5 3 pos Normal
-    doMinute 17 dT 8 3 pos Normal
-    doMinute 18 dT 8 3 pos Normal
-    doMinute 19 dT 3 3 pos Normal
-    doMinute 20 dT 3 3 pos Normal
-    doMinute 21 dT 5 3 pos Normal
-    doMinute 22 dT 5 3 pos Normal
-    doMinute 23 dT 5 3 pos Normal
-    doMinute 24 dT 5 3 pos Normal
-    doMinute 25 dT 8 3 pos Normal
-    doMinute 26 dT 8 3 pos Normal
-    doMinute 28 dT 5 3 pos Normal
-    doMinute 29 dT 8 3 pos Normal
+    doMinute 8 dT 3 3 pos Normal
+    doMinute 11 dT 8 3 pos Normal
+    doMinute 13 dT 3 3 pos Tank
+    doMinute 15 dT 3 3 pos Tank
 
 spawnsHive Hive3 dT pos = do
-    doMinute 2 dT 3 3 pos Normal
-    doMinute 7 dT 3 3 pos Normal
-    doMinute 8 dT 3 3 pos Normal
-    doMinute 11 dT 5 3 pos Normal
-    doMinute 14 dT 5 3 pos Normal
-    doMinute 15 dT 3 3 pos Normal
-    doMinute 17 dT 5 3 pos Normal
-    doMinute 18 dT 8 3 pos Normal
-    doMinute 19 dT 5 3 pos Normal
-    doMinute 20 dT 5 3 pos Normal
-    doMinute 21 dT 5 3 pos Normal
-    doMinute 22 dT 8 3 pos Normal
-    doMinute 23 dT 8 3 pos Normal
-    doMinute 24 dT 8 3 pos Normal
-    doMinute 25 dT 3 3 pos Normal
-    doMinute 26 dT 5 3 pos Normal
-    doMinute 27 dT 8 3 pos Normal
-    doMinute 28 dT 8 3 pos Normal
-    doMinute 29 dT 8 3 pos Normal
-
-spawnsHive Hive4 dT pos = do
-    doMinute 3 dT 3 3 pos Normal
+    doMinute 2 dT 5 3 pos Normal
+    doMinute 5 dT 8 3 pos Normal
     doMinute 8 dT 3 3 pos Normal
     doMinute 9 dT 5 3 pos Normal
-    doMinute 10 dT 5 3 pos Normal
+    doMinute 11 dT 5 3 pos Fast
+    doMinute 14 dT 8 3 pos Normal
+
+spawnsHive Hive4 dT pos = do
+    doMinute 3 dT 5 3 pos Normal
+    doMinute 6 dT 8 3 pos Normal
+    doMinute 8 dT 3 3 pos Normal
+    doMinute 9 dT 3 3 pos Fast
     doMinute 12 dT 5 3 pos Normal
-    doMinute 13 dT 8 3 pos Normal
-    doMinute 14 dT 5 3 pos Normal
-    doMinute 15 dT 3 3 pos Normal
-    doMinute 16 dT 3 3 pos Normal
-    doMinute 17 dT 5 3 pos Normal
-    doMinute 19 dT 5 3 pos Normal
-    doMinute 20 dT 3 3 pos Normal
-    doMinute 21 dT 5 3 pos Normal
-    doMinute 22 dT 8 3 pos Normal
-    doMinute 23 dT 3 3 pos Normal
-    doMinute 24 dT 5 3 pos Normal
-    doMinute 25 dT 8 3 pos Normal
-    doMinute 26 dT 5 3 pos Normal
-    doMinute 27 dT 8 3 pos Normal
-    doMinute 28 dT 8 3 pos Normal
-    doMinute 29 dT 8 3 pos Normal
+    doMinute 14 dT 5 3 pos Tank
 
 spawnsHive Hive5 dT pos = do
-    doMinute 3 dT 3 3 pos Normal
     doMinute 4 dT 3 3 pos Normal
-    doMinute 5 dT 3 3 pos Normal
-    doMinute 9 dT 8 3 pos Normal
-    doMinute 11 dT 5 3 pos Normal
-    doMinute 12 dT 3 3 pos Normal
-    doMinute 15 dT 3 3 pos Normal
-    doMinute 16 dT 5 3 pos Normal
-    doMinute 19 dT 8 3 pos Normal
-    doMinute 20 dT 5 3 pos Normal
-    doMinute 21 dT 5 3 pos Normal
-    doMinute 23 dT 8 3 pos Normal
-    doMinute 24 dT 8 3 pos Normal
-    doMinute 25 dT 3 3 pos Normal
-    doMinute 26 dT 5 3 pos Normal
-    doMinute 27 dT 8 3 pos Normal
-    doMinute 28 dT 5 3 pos Normal
-    doMinute 29 dT 8 3 pos Normal
+    doMinute 6 dT 3 3 pos Normal
+    doMinute 8 dT 3 3 pos Normal
+    doMinute 10 dT 8 3 pos Normal
+    doMinute 12 dT 3 3 pos Tank
+    doMinute 15 dT 8 3 pos Normal
 
 doMinute :: HasMany w [Time, Hp, Enemy, Position, Sprite, AnimatedSprite, Velocity, PathFinder, EntityCounter] => Int -> Float -> Float -> Float -> V2 Float -> EnemyType -> System w ()
 doMinute minute dT enemies waves pos etype = do
